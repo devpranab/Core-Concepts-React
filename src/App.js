e9d02c9f929d10860ba21ef66fcd98c9cf2529d9
pranav s <dev.pranabsk@gmail.com>
2022-05-04 11:17:17 +0530

    More props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import './App.css';
 
 function App() {
-  const personList = ["Rubel", "Sujit", "Salman"];
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'Pdf Reader', price: '$40.99'}
+  ]
   return ( 
     <div className="App">
-
       <h2>App Component</h2>
-    <Person name={personList[1]} title="Hasan"/>
-    <Person name="Ramij" title="Reja"/>
-    <Person name="Pranav" title="Sarkar"/>
-    <Person name="Siraj" title="Roy"/>
+      <Product product={products[0]}></Product>
+      <Product product={products[1]}></Product>
+
+    <Person name="Ramij" job="Marketer"></Person>
+    <Person name="Pranav" job="Designer"></Person>
+    <Person name="Siraj" job="Developer"></Person>
     </div>
   );
 }
 
-function Person(props){
-  const personStyle = {
-    border: '2px solid blue',
-    margin: '10px'
+function Product(props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+ 
   }
-  console.log(props);
+  const {name, price} = props.product;
+  return(
+    <div style={productStyle}>
+      <h2>Name: {name}</h2>
+      <h1>{price}</h1>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
+function Person(props){
   return (
-    <div style={personStyle}>
-      <h1>{props.name} {props.title}</h1>
-      <p>Something of Person</p>
+    <div style={{border: '2px solid gold', margin: '15px'}}>
+      <h3>{props.name}</h3>
+       <p>My Job: {props.job}</p>
     </div>
   )
 }
