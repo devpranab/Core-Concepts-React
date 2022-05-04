6fb73788ac525a7845c1d60557546da128eeb445
pranav s <dev.pranabsk@gmail.com>
2022-05-04 10:35:41 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 
 function App() {
+  const personList = ["Rubel", "Sujit", "Salman"];
   return ( 
     <div className="App">
 
       <h2>App Component</h2>
-    <Person/>
-    <Person/>
+    <Person name={personList[1]} title="Hasan"/>
+    <Person name="Ramij" title="Reja"/>
+    <Person name="Pranav" title="Sarkar"/>
+    <Person name="Siraj" title="Roy"/>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
   const personStyle = {
     border: '2px solid blue',
     margin: '10px'
   }
+  console.log(props);
   return (
     <div style={personStyle}>
-      <h1>Person Component</h1>
+      <h1>{props.name} {props.title}</h1>
       <p>Something of Person</p>
     </div>
   )
